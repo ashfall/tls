5d546b5e3836730b4ec30fd6c3faa531dd220461
ashfall <ashwini.oruganti@gmail.com>
2014-10-07 16:32:28 -0700

    Use constants to access parts of a message packet

--- tls/_buffer.py
+++ tls/_buffer.py
@@ -2,6 +2,10 @@ import struct
 
 from tls.record import ContentType
 
+HANDSHAKE_MESSAGE_LENGTH_START_INDEX = 1
+HANDSHAKE_BODY_START_INDEX = 5
+
+
 class HandshakeBuffer(object):
     def __init__(self, call_with_complete_handshake_message):
         self.handshake_bytes = b''
@@ -22,9 +26,9 @@ class HandshakeBuffer(object):
                     self.call_with_complete_handshake_message(self.handshake_bytes)
             else:
                 self.handshake_bytes = b''
-                handshake_message_type = fragment[0:1]
-                self.handshake_message_length = fragment[1:5]
-                handshake_body = fragment[5:]
+                handshake_message_type = fragment[:HANDSHAKE_MESSAGE_LENGTH_START_INDEX]
+                self.handshake_message_length = fragment[HANDSHAKE_MESSAGE_LENGTH_START_INDEX:HANDSHAKE_BODY_START_INDEX]
+                handshake_body = fragment[HANDSHAKE_BODY_START_INDEX:]
                 self.handshake_bytes += fragment
                 if len(handshake_body) < struct.unpack("!I", self.handshake_message_length)[0]:
                     self.waiting_for_next_fragment = True
