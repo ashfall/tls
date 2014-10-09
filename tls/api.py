ac8b3ba4fcc351bfbb772395bf52e22ffae921f6
ashfall <ashwini.oruganti@gmail.com>
2014-10-09 00:42:00 -0700

    Let's make the server send a ServerHello

--- tls/api.py
+++ tls/api.py
@@ -11,11 +11,11 @@ from tls.record import TLSPlaintext, ContentType
 
 class ClientTLS(object):
     """
-    The user wil create this and pass it the things needed to create a
+    The user will create this and pass to it the things needed to create a
     ClientHello object.
     """
 
-    def __init__(major_version, minor_version, gmt_unix_time, session_id='', cipher_suites, compression_methods, extensions=''):
+    def __init__(self, major_version, minor_version, gmt_unix_time, session_id='', cipher_suites, compression_methods, extensions=''):
         self.major_version = major_version
         self.minor_version = minor_version
         self.gmt_unix_time = gmt_unix_time      # TODO:Figure out how togenerate this
@@ -26,7 +26,7 @@ class ClientTLS(object):
         self.extensions = extensions
 
 
-    def start(write_to_wire_callback, wire_close_callback, verify_callback=None):
+    def start(self, write_to_wire_callback, wire_close_callback, verify_callback=None):
         """
         First, this creates a ClientHello message and writes it to the wire.
         Then, it creates a Connection object and passes to it the ClientHello
@@ -72,7 +72,57 @@ class ClientTLS(object):
         # Write this to wire.
         write_to_wire_callback(tls_plaintext_record.as_bytes())
 
-        # Create a Connection object and pass the Client
+        # Create a Connection object and pass the ClientHello-Handshake struct to it.
+        conn = Connection(write_to_wire_callback)
+        conn.handshake_msg_store[handshake.msg_type] = handshake
+        return conn
+
+
+
+class ServerTLS(object):
+    """
+    The user will create this and pass to it the things needed to create a
+    ServerHello object.
+    """
+    def __init__(self):
+        pass
+
+    def start(self, write_to_wire_callback, verify_callback=None):
+        conn = Connection(write_to_wire_callback)
+
+
+
+
+
+
+
+
+
+class Connection(object):
+
+    def __init__(self, write_to_wire_callback):
+        self.write_to_wire_callback = write_to_wire_callback
+        self.handshake_msg_store = {}
+
+    def structure_bytes_from_wire(self, input_bytes):
+        """
+        Receive data and build a structure out of it.
+        """
+        # TODO: Buffering of fragmented messages goes here.
+
+        tls_plaintext_record = parse_tls_plaintext(input_bytes)
+        if tls_plaintext_record.type == ContentType.HANDSHAKE:
+            handshake_struct = parse_handshake_struct(tls_plaintext_record.fragment)
+            self.handshake_msg_store[handshake_struct.msg_type] = handshake_struct
+
+
+    def construct_bytes_from_application_and_write_to_wire(self, output):
+        """
+        Encrypt application data and send. This is only useful in APP_DATA state.
+        """
+        encrypted_output = encrypt_output_bytes_with_negotiated_cipher_suite
+        self.write_to_wire_callback(encrypted_output)
+
 
 
 
